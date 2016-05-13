5cb5bf61773af9dab24f4f96d68b340a95629b32
JeanSchwalenberg <jeanschwalenberg@gmail.com>
2016-05-12 20:27:34 -0600

    mapped MyRoute

--- UrlsAndRoutes/App_Start/RouteConfig.cs
+++ UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -5,19 +5,10 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace UrlsAndRoutes
-{
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+namespace UrlsAndRoutes {
+    public class RouteConfig {
+        public static void RegisterRoutes(RouteCollection routes) {
+            routes.MapRoute("MyRoute", "{controller}/{action}");
         }
     }
 }
